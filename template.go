66da132f8df2babf505868eebb59deaaf69bbf97
Vinnicyus Gracindo <vini.gracindo@gmail.com>
2022-05-19 16:46:53 -0300

    Exercício - Condicionais - 19/05/2022 - Tarde

---
+++ template.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+/** Exercise 1
+**/
+
+func doExercise1() {
+
+}
+
+/** Exercise 2
+**/
+
+func doExercise2() {
+
+}
+
+/** Exercise 3
+**/
+
+func doExercise3() {
+
+}
+
+/** Exercise 4
+
+**/
+
+func doExercise4() {
+
+}
+
+func main() {
+	fmt.Println("\nExercise 1...")
+	doExercise1()
+	fmt.Println("\n-------------------")
+	fmt.Println("\nExercise 2...")
+	doExercise2()
+	fmt.Println("\n-------------------")
+	fmt.Println("\nExercise 3...")
+	doExercise3()
+	fmt.Println("\n-------------------")
+	fmt.Println("\nExercise 4...")
+	doExercise4()
+}
